078340a480108d0785fb21540143134e1305e8a9
SiMoG97 <echaaranimohamed@gmail.com>
2023-10-22 23:31:14 +0100

    Task-8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""hello_route"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+# storage.all(...)
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_storage(exception=None):
+    """Closes the storage session"""
+    storage.close()
+
+
+@app.route("/states_list",  strict_slashes=False)
+def number_odd_or_even():
+    """shows an html page with a list of states"""
+
+    states = sorted(list(storage.all(State).values()), key=lambda
+                    x: x.name)
+    teardown_storage()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
